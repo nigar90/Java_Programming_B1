23107b629d6ede390259c73b36202d25f6c2bd14
nigar90 <nigar.rahimova90@gmail.com>
2023-04-08 03:25:33 -0400

    day_24_office_hours_1

---
+++ src/day25_methods/FixFormat.java
@@ -0,0 +1,26 @@
+package day25_methods;
+
+
+    /*
+    Fix Format
+
+create a method will accept a String and return a String in proper format. Proper format is:
+First character starting as uppercase and the rest as lowercase
+
+Ex:
+	Input:
+		jamES
+
+	Output:
+		James
+
+     */
+
+public class FixFormat {
+
+    public static String fixFormat(String str){ // JAMES --> J.toUpperCase() | AMES.toLowerCase();
+        return (str.charAt(0) + "").toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+
+}
